34fdc122ac47336bbecc71cc33f7f63b55abb5aa
wahidAhmad <thesharifi.wahid@gmail.com>
2024-05-12 18:41:48 +0430

    add mode
    add day and night mode to the website

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
-// select the elements you want to observe
-var reveals = document.querySelectorAll(".skill-per");
+/*
+   theme
+*/
+const rootTheme = document.querySelector(":root");
+const themeBtn = document.querySelector("#theme");
+const relImg = document.querySelectorAll(".photo")
+let isDarkTheme = false;
 
-// create an observer with a callback function
-var observer = new IntersectionObserver(function (entries) {
-   // loop through the entries
-   for (var entry of entries) {
-      // check if the entry is intersecting
+const updateThemeProperties = (properties, imgFilter) => {
+    Object.keys(properties).forEach((key) => {
+        rootTheme.style.setProperty(key, properties[key]);
+    });
+    relImg.forEach((img) => {
+        img.style.filter = imgFilter;
+    });
+};
+
+themeBtn.addEventListener("click", () => {
+    const themeProperties = isDarkTheme ? {
+        "--bgColorTop": "radial-gradient(circle at 25% 50%, rgb(253 255 0) 0%, rgb(255 206 69) 50%)",
+        "--bgColor": "#fff",
+        "--textColor": "#000",
+        "--aHover": "#dcdcdc",
+    } : {
+        "--bgColorTop": "radial-gradient(circle at 25% 50%, rgba(96, 96, 96, 1) 14%, rgba(24, 32, 49, 1) 100%)",
+        "--bgColor": "#000",
+        "--textColor": "#fff",
+        "--aHover": "#555050",
+    };
+    const imgFilter = isDarkTheme ? "brightness(0)" : "brightness(1)";
+
+    updateThemeProperties(themeProperties, imgFilter);
+    isDarkTheme = !isDarkTheme;
+});
+/*
+   skill bar
+*/
+let reveals = document.querySelectorAll(".skill-per");
+
+let observer = new IntersectionObserver(function (entries) {
+   for (let entry of entries) {
       if (entry.isIntersecting) {
-         // add the active class to the target element
          entry.target.classList.add("active");
       } else {
-         // remove the active class from the target element
          entry.target.classList.remove("active");
       }
    }
 });
 
-// loop through the elements and observe them
-for (var reveal of reveals) {
+for (let reveal of reveals) {
    observer.observe(reveal);
 }
